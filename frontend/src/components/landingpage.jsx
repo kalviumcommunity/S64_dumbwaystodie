3ed210920e208c1988c1b9f2b1603f6a77417d53
gastrotope427 <gaurang.mishra.s64@kalvium.community>
2025-02-20 15:59:51 +0530

    milestone8 done

---
+++ frontend/src/components/landingpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to ASAP Project</h1>
+      <p>This is our landing page for our innovative idea.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
